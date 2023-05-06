31022729c6df644db453f53fea53abe415efef54
SADDIK <soufiane.saddik@sogeti.com>
2023-05-06 20:01:56 +0200

    Affichage de la liste des patients

--- src/main/java/com/example/patientsmvc/web/PatientController.java
+++ src/main/java/com/example/patientsmvc/web/PatientController.java
@@ -1,7 +1,22 @@
 package com.example.patientsmvc.web;
 
+import com.example.patientsmvc.entities.Patient;
+import com.example.patientsmvc.repositories.PatientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class PatientController {
+    private PatientRepository patientRepository;
+    @GetMapping(path = "/index")
+    public  String patients(Model model){
+        List<Patient> patients=patientRepository.findAll();
+        model.addAttribute("listPatients",patients);
+        return "patients";
+    }
 }
