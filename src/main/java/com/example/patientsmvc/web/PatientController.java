8c89775cc4867818e7c496244c966e9989bf9ad5
SADDIK <soufiane.saddik@sogeti.com>
2023-05-08 21:17:58 +0200

    delete

--- src/main/java/com/example/patientsmvc/web/PatientController.java
+++ src/main/java/com/example/patientsmvc/web/PatientController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,5 +28,15 @@ public class PatientController {
         model.addAttribute("currentPage",page);
         model.addAttribute("keyword",keyword);
     return "patients";
+    }
+
+    @GetMapping("/delete")
+    public  String delete(Long id, String keyword,int page){
+        patientRepository.deleteById(id);
+        return  "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public  String home(){
+        return  "redirect:/index";
     }
 }
